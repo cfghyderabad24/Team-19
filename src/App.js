4136e3b1a00bc63b956e77668fc522506025ed55
SriLakshmi-3 <2100032462@kluniversity.in>
2024-06-23 03:55:33 +0530

    this is my mern first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-// src/App.js
-import React from 'react';
-import Login from './Login';
+import React, { useState } from 'react';
 import './App.css';
+import Login from './components/login/login';
+import StudentList from './components/StudentList';
+import StudentDetails from './components/StudentDetails';
+import students from './components/data/students';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [selectedStudent, setSelectedStudent] = useState(null);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleSelectStudent = (student) => {
+    setSelectedStudent(student);
+  };
+
+  const handleBack = () => {
+    setSelectedStudent(null);
+  };
+
+  if (!isLoggedIn) {
+    return <Login onLogin={handleLogin} />;
+  }
+
   return (
     <div className="App">
-      <Login />
+      {selectedStudent ? (
+        <StudentDetails student={selectedStudent} onBack={handleBack} />
+      ) : (
+        <StudentList students={students} onSelectStudent={handleSelectStudent} />
+      )}
     </div>
   );
 }
---
+++ src/components/login/login.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './login.css';
+
+
+import user_icon from '../assets/person.png'
+import password_icon from '../assets/password.png'
+
+const Login=({ onLogin })=>{
+
+
+    return(
+        <div className ='container'>
+            <div className="header">
+                <div className="text">Login</div>
+                <div className="underline"></div>
+            </div>
+            <div className ="inputs">
+
+                <div className="input">
+                    <img src={user_icon} alt=""/>
+                    <input type="text" placeholder="User Name"/>
+                </div>
+                <div className="input">
+                    <img src={password_icon} alt=""/>
+                    <input type="password" placeholder="Password"/>
+                </div>
+            
+            </div>
+            
+            
+            <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
+            <div className="submit-container">
+                <div className="submit" onClick={onLogin}>Login</div>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Login;
